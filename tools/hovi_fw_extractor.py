80386f457a3f26b381ccb4f465d0b9cbb6951f36
Valentin B <valentin.be@protonmail.com>
2020-05-05 14:11:29 +0200

    Add script for extracting TSEC fw from Package1

---
+++ tools/hovi_fw_extractor.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from pathlib import Path
+from struct import unpack
+import sys
+
+
+def main(package1_path):
+    package1_path = Path(package1_path)
+    output_path = Path('tsec_fw.bin')
+
+    with open(package1_path, 'rb') as f:
+        package1 = f.read()
+    
+    hovi_common_seed_position = package1.find(b'HOVI_COMMON_01\x00\x00')
+    if hovi_common_seed_position == -1:
+        raise RuntimeError('Malformed Package1 binary!')
+
+    # Parse the Key Data table.
+    key_data_start = hovi_common_seed_position - 0x60
+    key_data_end = key_data_start + 0x100
+    (
+        boot_size,
+        keygenldr_size,
+        keygen_size,
+        securebootldr_size,
+        secureboot_size
+    ) = unpack('16s16s16s16s16s16s16sIIIII124x', package1[key_data_start:key_data_end])[7:]
+
+    tsec_fw_size = boot_size + keygenldr_size + keygen_size + securebootldr_size + secureboot_size
+    tsec_fw_start = key_data_start - boot_size
+    tsec_fw_end = tsec_fw_start + tsec_fw_size
+
+    with open(output_path, 'wb') as f:
+        f.write(package1[tsec_fw_start:tsec_fw_end])
+
+
+if __name__ == '__main__':
+    args = sys.argv[1:]
+    if len(args) != 1:
+        raise RuntimeError('Usage: package1_path')
+
+    main(args[0])
