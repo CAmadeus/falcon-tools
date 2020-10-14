9aa4265f38893d9c7b1a707c826afa037035c727
Valentin B <valentin.be@protonmail.com>
2020-10-14 21:04:14 +0200

    Don't forget to extract the last 0x100 firmware bytes

--- tools/hovi_fw_extractor.py
+++ tools/hovi_fw_extractor.py
@@ -6,16 +6,16 @@ from struct import unpack
 import sys
 
 
-def main(package1_path):
+def extract_tsec_fw(package1_path):
     package1_path = Path(package1_path)
-    output_path = Path('tsec_fw.bin')
+    output_path = Path("tsec_fw.bin")
 
-    with open(package1_path, 'rb') as f:
+    with open(package1_path, "rb") as f:
         package1 = f.read()
-    
-    hovi_common_seed_position = package1.find(b'HOVI_COMMON_01\x00\x00')
+
+    hovi_common_seed_position = package1.find(b"HOVI_COMMON_01\x00\x00")
     if hovi_common_seed_position == -1:
-        raise RuntimeError('Malformed Package1 binary!')
+        raise RuntimeError("Malformed Package1 binary!")
 
     # Parse the Key Data table.
     key_data_start = hovi_common_seed_position - 0x60
@@ -26,9 +26,9 @@ def main(package1_path):
         keygen_size,
         securebootldr_size,
         secureboot_size
-    ) = unpack('16s16s16s16s16s16s16sIIIII124x', package1[key_data_start:key_data_end])[7:]
+    ) = unpack("16s16s16s16s16s16s16sIIIII124x", package1[key_data_start:key_data_end])[7:]
 
-    tsec_fw_size = boot_size + keygenldr_size + keygen_size + securebootldr_size + secureboot_size
+    tsec_fw_size = boot_size + 0x100 + keygenldr_size + keygen_size + securebootldr_size + secureboot_size
     tsec_fw_start = key_data_start - boot_size
     tsec_fw_end = tsec_fw_start + tsec_fw_size
 
@@ -36,9 +36,9 @@ def main(package1_path):
         f.write(package1[tsec_fw_start:tsec_fw_end])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     args = sys.argv[1:]
     if len(args) != 1:
-        raise RuntimeError('Usage: package1_path')
-
-    main(args[0])
+        print("Usage: package1_path")
+    else:
+        extract_tsec_fw(args[0])
