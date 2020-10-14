c2881f506dbab8b52bfdd73978b11662f0c9bcb1
Valentin B <valentin.be@protonmail.com>
2020-10-14 21:20:37 +0200

    Fix Boot auth hash generation

--- tools/hovi_keygenldr_auth_boot.py
+++ tools/hovi_keygenldr_auth_boot.py
@@ -1,38 +1,51 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
-import errno
-import os
-import struct
+from binascii import hexlify, unhexlify
 import sys
-import binascii
 
 from Crypto.Cipher import AES
-from Crypto.Hash import CMAC
-
-import pyaes
-
-with open("blob0_auth_key.bin", 'rb') as f:
-    blob0_auth_key = f.read()
-    print(binascii.hexlify(blob0_auth_key))
-
-blob0_auth_aes = pyaes.AES(blob0_auth_key)
 
 
-with open("blob0.bin", "rb") as f:
-    blob0 = f.read()
+def sxor(x, y):
+    return bytearray(a ^ b for a, b in zip(x, y))
 
-blob0_size = len(blob0)
-print(blob0_size)
 
-size_value = ((blob0_size & 0xff) << 8 | (blob0_size & 0xff00) >> 8) << 0x10 | ((blob0_size & 0xff0000) >> 0x10) << 8 | blob0_size >> 0x18
+def hswap(size):
+    return (
+        ((size & 0x000000FF) << 0x8
+         | (size & 0x0000FF00) >> 0x8) << 0x10
+        | ((size & 0x00FF0000) >> 0x10) << 0x8
+        | (size & 0xFF000000) >> 0x18
+    )
 
-print(hex(size_value))
 
-size_key = bytearray(0x10)
+def encrypt_buffer(buffer, size, key):
+    buffer[0xC:] = hswap(size).to_bytes(4, "little")
+    return AES.new(key, AES.MODE_ECB).encrypt(buffer)
 
-size_key[0xC:] = size_value.to_bytes(4, 'little')
 
-cobj = CMAC.new(blob0_auth_key, ciphermod=AES)
-cobj.update(size_key)
-cobj.update(blob0)
-print(cobj.hexdigest())
+def generate_cmac(boot_path, key):
+    with open(boot_path, "rb") as f:
+        boot = f.read()
+
+    # Craft the signature key to be used as the IV.
+    sig_key = bytearray(AES.block_size)
+    sig_key = encrypt_buffer(sig_key, len(boot), key)
+
+    # Calculate the AES-CMAC hash over Boot code.
+    ciphertext = sig_key
+    for i in range(0, len(boot), AES.block_size):
+        block_cipher = sxor(boot[i:i + AES.block_size], ciphertext)
+        ciphertext = AES.new(key, AES.MODE_ECB).encrypt(block_cipher)
+
+    return ciphertext
+
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+    if len(args) != 2:
+        print("Usage: boot_path key")
+    else:
+        cmac = generate_cmac(args[0], unhexlify(args[1]))
+        print(f"Hash:  {hexlify(cmac).decode('ascii')}")
