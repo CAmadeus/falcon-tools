2a1edbafa1f6607a021b5c875bbd3c4fa0e196d6
Valentin B <valentin.be@protonmail.com>
2020-10-14 21:48:20 +0200

    Fix argument parsing

--- tools/decrypt_blob2.py
+++ tools/decrypt_blob2.py
@@ -11,8 +11,6 @@ def decrypt_blob2(blob2_path, output_path, key, iv=None):
     with open(blob2_path, "rb") as f:
         blob2 = f.read()
 
-    if not iv:
-        iv = unhexlify("00000000000000000000000000000000")
     blob2_dec = AES.new(key, AES.MODE_CBC, iv).decrypt(blob2)
 
     with open(output_path, "wb") as f:
@@ -24,4 +22,5 @@ if __name__ == "__main__":
     if len(args) < 3:
         print("Usage: blob2_path output_path key [iv]")
     else:
-        decrypt_blob2(*args)
+        decrypt_blob2(args[0], args[1], unhexlify(args[2]), unhexlify(
+            args[3]) if args[3] else unhexlify("00000000000000000000000000000000"))
